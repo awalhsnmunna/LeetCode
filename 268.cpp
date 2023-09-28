ed43d09877f3809e461a5eef02a76b83db519236
Munna <awalhsnmunna@gmail.com>
2023-09-28 18:11:01 +0600

    Code

---
+++ 268.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+    int totalSum = 0;
+    int n = nums.size();
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        totalSum += nums[i];
+    }
+
+    int AllNSum = (n * (n + 1)) / 2;
+
+    return AllNSum-totalSum;
+    
+    }
+};
